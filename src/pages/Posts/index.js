9449ad86cba061317d1789db2ea9d0adb9fb1012
phbai <48466846@qq.com>
2018-10-09 11:09:20 +0800

    修改Posts为函数组件

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,24 +1,20 @@
 import styles from './index.css';
-import React, { Component } from 'react'
 import { connect } from 'dva';
 import { Pagination } from 'antd';
 import router from 'umi/router';
 import PostList from '../../components/PostList'
 
-class Posts extends Component {
-  onPageChange = (page) => {
+const Posts = ({ posts, currentPage }) => {
+  function onPageChange(page) {
     router.push(`/posts?page=${page}`);
   }
 
-  render() {
-    const { posts, currentPage } = this.props;
-    return (
-      <div className={styles.list}>
-        <PostList posts={posts} />
-        <Pagination defaultCurrent={1} total={50} current={currentPage} onChange={this.onPageChange}/>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.list}>
+      <PostList posts={posts} />
+      <Pagination defaultCurrent={1} total={50} current={currentPage} onChange={onPageChange}/>
+    </div>
+  )
 }
 
 
